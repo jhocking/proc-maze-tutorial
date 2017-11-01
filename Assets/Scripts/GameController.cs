34b63ececadb29dab807b1e819fc99659835c220
Joseph Hocking <jhocking@newarteest.com>
2017-11-01 18:08:32 -0500

    require maze constructor

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MazeConstructor))]
+
 public class GameController : MonoBehaviour {
 	private MazeConstructor _generator;
 
 	// Use this for initialization
 	void Start() {
 		_generator = GetComponent<MazeConstructor>();
+
 		_generator.GenerateNewMaze(13, 15);
 	}
 
