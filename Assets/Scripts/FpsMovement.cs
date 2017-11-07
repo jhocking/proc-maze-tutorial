543f336ced8a4fe2d638e0d37b598b25159e0489
Joseph Hocking <jhocking@newarteest.com>
2017-11-07 17:40:28 -0600

    mit license

--- Assets/Scripts/FpsMovement.cs
+++ Assets/Scripts/FpsMovement.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/*
+ * written by Joseph Hocking 2017
+ * released under MIT license
+ * text of license https://opensource.org/licenses/MIT
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * written by Joseph Hocking 2017
+ * released under MIT license
+ * text of license https://opensource.org/licenses/MIT
+ */
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
